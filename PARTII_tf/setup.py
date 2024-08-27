8da1246d529952fdfae358eadd21a52abb447618
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2024-08-27 11:26:09 -0400

    fix setup

--- PARTII_tf/setup.py
+++ PARTII_tf/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FlowUQ",
-    version="v0.0-beta", 
+    version="v0.1.0-beta", 
     packages=find_packages(),
     install_requires=[
         "numpy",
